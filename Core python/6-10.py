539047745b3716e9a9e9b3b6444385280837630b
pngisnotgif <pngisnotgif@qq.com>
2013-04-13 19:12:08 +0800

    add 6-10

---
+++ Core python/6-10.py
@@ -0,0 +1,18 @@
+# 6-10
+
+def capital_reverse(s):
+    new_s = ''
+    for i in s:
+        if i.islower() :
+            new_s += i.upper()
+        elif i.isupper() :
+            new_s += i.lower()
+        else:
+            new_s += i
+
+    print s,'==>',new_s
+
+if __name__=='__main__':
+    testcases = ['Mr.Ed', 'mR.eD', 'abc', '', ' ', 'aBc', 'A', 'AB']
+    for i in testcases:
+        capital_reverse(i)
