a649fe09e02c481b6fd996c83135815216296e04
pngisnotgif <pngisnotgif@qq.com>
2013-05-04 11:42:32 +0800

    add support of timestamp
    added timestamp and improved words

--- Core Python Programming/7-5.py
+++ Core Python Programming/7-5.py
@@ -1,33 +1,50 @@
 #!/usr/bin/env python
 
 # 7-5
-# from example 7.1
+
+# v1.1 - add timestamp
+# v1.0 - from example 7.1
+
+import time
 
 db = {}
 
+def save_timestamp():
+    db['timestrmp'] = time.time()
+    
 def newuser():
-    prompt = 'login desired: '
+    prompt = 'Login desired: '
     while True:
         name = raw_input(prompt)
         if db.has_key(name):
-            prompt = 'name taken, try another: '
+            prompt = 'Warning: name taken, try another login: '
             continue
         else:
             break
-    pwd = raw_input('passwd: ')
+    
+    pwd = raw_input('Passwd: ')
     db[name] = pwd
+    save_timestamp()
 
 def olduser():
-    name = raw_input('login: ')
-    pwd = raw_input('passwd: ')
+    name = raw_input('Login: ')
+    pwd = raw_input('Passwd: ')
     passwd = db.get(name)
     if passwd == pwd:
-        print 'welcome back, ', name
+        print 'Welcome back,', name, '!'
+        if (time.time()- db['timestrmp'])<4*60*60:  # less than 4 hours
+            print 'You already logged in at: %s.' \
+                  %(time.strftime('%x %X',time.localtime(db['timestrmp'])))
+            # change timestamp in seconds since the epoch to 9-tuple, and then to string
+        save_timestamp()
     else:
-        print 'login incorrect.'
+        print 'Warning: login or passwd incorrect.'
+    raw_input()
 
 def showmenu():
     prompt ="""
+        MENU
+      ========
 (N)ew User Login
 (E)xisting User Login
 (Q)uit
@@ -42,14 +59,18 @@ Enter choice: """
                 choice = raw_input(prompt).strip()[0].lower()
             except (EOFError, KeyboardInterrupt):
                 choice = 'q'
-            print '\nYou picked: [%s]'%choice
+                
+            print 'You picked: [%s]'%choice
+            
             if choice not in 'neq':
-                print 'invalid option, try again'
+                print 'Warning: invalid option, try again.'
+                raw_input()
             else:
                 chosen = True
 
         if choice == 'q':
             done = True
+            print 'Bye!'
         if choice == 'n':
             newuser()
         if choice == 'e':
