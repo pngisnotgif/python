20740a3a5146d5c671c0d62fa47982ae8794e059
pngisnotgif <pngisnotgif@qq.com>
2013-05-05 11:55:55 +0800

    add sub-menu: adminstrate
    function: delete and list user.

--- Core Python Programming/7-5.py
+++ Core Python Programming/7-5.py
@@ -2,6 +2,7 @@
 
 # 7-5
 
+# v1.2 - add sub-menu: administrate(del account, and list users)
 # v1.1 - add timestamp
 # v1.0 - from example 7.1
 
@@ -45,12 +46,75 @@ def olduser():
         print 'Warning: login or passwd incorrect.'
     raw_input()
 
+def delUser():
+    if db.keys():
+        prompt = 'Login to be deleted: '
+        name = raw_input(prompt)
+        if name in db:
+            print 'Detail of %s: \n'%(name), db[name]
+            confirm = raw_input('Delete this account? Y or N? ').strip().lower()
+            if confirm == 'y':
+                del db[name]
+                print 'Account: %s is deleted.'%(name)
+            else:
+                print 'No account is deleted.'
+        else:
+            print 'No such an account.'
+    else:
+        print 'Empty accounts.'
+    raw_input()
+
+def listAllUser():
+    if db.keys():
+        for user in db:
+            print 'name:', user,
+            print 'password:', db[user]['pwd']
+    else:
+        print 'No accounts.'
+    raw_input()
+
+def administrate():
+    '''
+        sub-menu of main menu.
+
+    '''
+    
+    prompt = '''
+        Administrate
+        ============
+(D)elete an account
+(L)ist all acounts
+(R)eturn
+
+Enter your choice: '''
+
+    ret = False
+    while not ret:
+        try:
+            choice = raw_input(prompt).strip()[0].lower()
+        except (EOFError, KeyboardInterrupt):
+            choice = 'r'    # how to directly return to showmenu()?
+
+        print 'You picked: [%s]'%choice
+        if choice not in 'dlr':
+            print 'Warning: invalid option, try again.'
+            raw_input()
+        else:
+            if choice == 'd':
+                delUser()
+            if choice == 'l':
+                listAllUser()
+            if choice == 'r':
+                ret = True
+
+    
 def showmenu():
     prompt ="""
         MENU
       ========
 (N)ew User Login
 (E)xisting User Login
+(A)dministrate
 (Q)uit
 
 Enter choice: """
@@ -66,7 +130,7 @@ Enter choice: """
                 
             print 'You picked: [%s]'%choice
             
-            if choice not in 'neq':
+            if choice not in 'neqa':
                 print 'Warning: invalid option, try again.'
                 raw_input()
             else:
@@ -79,6 +143,8 @@ Enter choice: """
             newuser()
         if choice == 'e':
             olduser()
+        if choice == 'a':
+            administrate()
 
 if __name__=='__main__':
     showmenu()
