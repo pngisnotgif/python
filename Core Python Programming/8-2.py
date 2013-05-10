ac96cd3b86e4517142c8fe804699546b3ee57109
pngisnotgif <pngisnotgif@qq.com>
2013-05-10 21:55:09 +0800

    add 8-2, 8-3
    range

---
+++ Core Python Programming/8-2.py
@@ -0,0 +1,9 @@
+# 8-2
+
+f = 2
+t = 26
+i = 4
+for x in range(f,t,i):
+    print x,',',
+print x+i
+
---
+++ Core Python Programming/8-3.py
@@ -0,0 +1,10 @@
+# 8-3
+
+listA = [x for x in range(10)]
+print listA
+
+listB = [x for x in range(3, 18+1, 3)]
+print listB
+
+listC = [x for x in range(-20, 860+1, 220)]
+print listC
