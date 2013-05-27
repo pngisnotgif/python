0c27f6648875e2b7d5180c0c728c07212ecbf6f4
pngisnotgif <pngisnotgif@qq.com>
2013-05-27 23:07:43 +0800

    add comment: how to sort in type order?

--- Core Python Programming/9-8.py
+++ Core Python Programming/9-8.py
@@ -7,6 +7,10 @@ module = __import__(module_name)
 module_dict = module.__dict__
 
 count = 25
+
+# how to sort in type?
+# for i in sorted(module_dict.keys(), key=lambda x:str(type(x))):
+
 for i in sorted(module_dict.keys()):
     print 'Name: ', i, 'Type:', type(module_dict[i]), 'Value:', module_dict[i]
     count -= 1
