f03e69cbcae99c1102264b60ea933e1c2e59d907
pngisnotgif <pngisnotgif@qq.com>
2013-06-02 11:35:06 +0800

    add 9-21: zip

---
+++ Core Python Programming/9-21.py
@@ -0,0 +1,36 @@
+# 9-21
+
+import glob
+import zipfile
+import os
+
+pyfiles = glob.glob('*.py')   # find all py files in current dir.
+zip_file_name = 'python.zip'
+txtfile = glob.glob('*.txt')
+out_dir = '9-21'
+
+def compress_all_py(file_list):
+    f_out = zipfile.ZipFile(zip_file_name,'w')
+    for f in file_list:
+        f_out.write(f)
+    f_out.close()
+
+def append_arch(filename):
+    f_out = zipfile.ZipFile(zip_file_name,'a')
+    f_out.write(filename)
+    f_out.close()
+
+def extract_arch(filename):
+    f_in = zipfile.ZipFile(zip_file_name)
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    f_in.extract(filename,out_dir)
+    f_in.close()
+
+if __name__=='__main__':
+    compress_all_py(pyfiles)    # compress all py files
+    
+    for txt in txtfile:         # compress txt files into zip
+        append_arch(txt)
+
+    extract_arch('9-21.py')
