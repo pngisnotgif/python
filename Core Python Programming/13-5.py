b4f2208ea1d3e4e21090c372293ca90d90bc7678
pngisnotgif <pngisnotgif@qq.com>
2013-06-08 20:25:26 +0800

    add 13-5: constitution

---
+++ Core Python Programming/13-5.py
@@ -0,0 +1,80 @@
+# 13-5 and 13-6
+
+class Point(object):
+    '''
+        point class
+    '''
+
+    points = 0    
+
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
+        print 'point ({x},{y}) created.'.format( x=self.__x, y=self.__y )
+        Point.points += 1
+        # print '%s.%s called.'%(self.__class__, __name__) # how to print this function's name?
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def __str__(self):
+        return "(%d, %d)"%(self.__x, self.__y)
+
+    def __repr__(self):
+        return "Point: (%d, %d)"%(self.__x, self.__y)
+
+    @classmethod    # must use this decorator operator
+    def countPoint(self):   # self must be explicit
+        '''
+        class method: calcule how many points there are
+        '''
+        return Point.points
+
+    def __del__(self):
+        Point.points -= 1
+
+
+class Line(object):
+    '''
+    line class
+    '''
+
+    def __init__(self, start_point=Point(), end_point=Point()):
+        print 'Line.__init__ called.'
+        self.start = start_point
+        self.end = end_point
+
+    def __str__(self):    
+        return "((%d,%d)-->(%d,%d))"%(self.start.get_x(), \
+                                     self.start.get_y(), \
+                                     self.end.get_x(), \
+                                     self.end.get_y())
+
+# WTF: when executing this file, definition of class Line will call Point.__init__ twice.
+
+def test_point():
+    point_a = Point(1,2)
+    point_b = Point()
+    point_c = Point(y=3)
+    point_d = Point(x=4)
+    
+    print point_c
+
+    print Point.points
+
+def test_line():
+    p1 = Point(1,1)
+    p2 = Point(2,2)
+    line1 = Line(p1, p2)
+    print line1
+
+    line2 = Line(Point(1,2),Point(3,4)) # Line((1,2),(3,4)) is wrong: \
+                # AttributeError: 'tuple' object has no attribute 'get_x'
+    print line2
+    
+if __name__=='__main__':
+    test_line()
+    
